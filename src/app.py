c5dbddbcf2039e8c8177a1ffd31204ac29313b83
Ewald Verhoeven <ewaldverhoeven@hotmail.com>
2022-08-25 17:00:46 +0200

    add unit tests

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 from measurement.measures import Distance
-# distance = Distance(yd=100)
-# print(distance)
 
-def get_mil(delta, system='metric', distance=100):
+def get_mil(delta: float, system: str, distance: int):
     #BASE_RULE: 1 mil == 3.6 inch (on 100 yards)
     BASE_RULE_DIST = 100
     BASE_RULE_DEVI = 3.6
@@ -17,16 +15,11 @@ def get_mil(delta, system='metric', distance=100):
         diff_factor = distance.yd / BASE_RULE_DIST
         return round(delta.inch / BASE_RULE_DEVI * diff_factor, 2)
 
-print(get_mil(5, system='imperial', distance=150))
-print(get_mil(5, system='metric', distance=150))
-# print(get_mil(3.6, 100, system='imperial'))
-
-
-def get_moa(delta, system='metric', distance=100):
+def get_moa(delta: float, system: str, distance: int):
     # BASE_RULE: 1 moa == 1 inch (on 100 yards)
     BASE_RULE_DIST = 100
     BASE_RULE_DEVI = 1
-
+    print(f'system = {system}')
     if system == 'imperial':
         diff_factor = distance / BASE_RULE_DIST
         return round(delta / BASE_RULE_DEVI* diff_factor, 2)
@@ -37,19 +30,20 @@ def get_moa(delta, system='metric', distance=100):
         diff_factor = distance.yd / BASE_RULE_DIST
         return round(delta.inch /BASE_RULE_DEVI * diff_factor, 2)
 
-# print(get_moa(5, distance=150))
-
-def get_zero_adjustments(x_deviation: float, y_deviation: float, output: str, distance: int = 100, system='metric'):
+def get_adjustments(x_deviation: float, y_deviation: float, output: str, distance: int = 100, system='metric'):
     if system != 'metric' and system != 'imperial':
         raise ValueError("system has to be either 'metric' or 'imperial'")
 
     if output == 'MOA':
-        x = get_moa(x_deviation, distance, system)
-        y = get_moa(y_deviation, distance, system)
+        x = get_moa(x_deviation, system, distance)
+        y = get_moa(y_deviation, system, distance)
     elif output == 'MRAD':
-        x = get_mil(x_deviation, distance, system)
-        y = get_mil(y_deviation, distance, system)
+        x = get_mil(x_deviation, system, distance)
+        y = get_mil(y_deviation, system, distance)
     else:
         raise ValueError("Output must be either 'MOA' or 'MRAD'")
 
     return {'sight-type':output, 'x':x, 'y':y}
+
+print(get_adjustments(-5, -10, 'MRAD'))
+print(get_adjustments(-5, -10, 'MOA'))
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.app import get_moa, get_mil
+from src.app import get_moa, get_mil, get_adjustments
 from hypothesis import given, strategies as st
 
 # @given(
@@ -12,25 +12,33 @@ from hypothesis import given, strategies as st
 #     assert type(result) == float
 
 def test_get_moa_low():
-    assert get_moa(-10, system='imperial') == -10
-    assert get_moa(-10, system='metric') == -4.31
+    assert get_moa(-10, system='imperial', distance=100) == -10
+    assert get_moa(-10, system='metric', distance=100) == -4.31
 
 def test_get_moa_high():
-    assert get_moa(5, system='imperial') == 5
-    assert get_moa(5, system='metric') == 2.15
+    assert get_moa(5, system='imperial', distance=100) == 5
+    assert get_moa(5, system='metric', distance=100) == 2.15
 
 def test_get_moa_alt_distance():
     assert get_moa(5, system='imperial', distance=150) == 7.5
     assert get_moa(5, system='metric', distance=150) == 3.23
 
 def test_get_mil_low():
-    assert get_mil(-10, system='imperial') == -2.78
-    assert get_mil(-10, system='metric') == -1.2
+    assert get_mil(-10, system='imperial', distance=100) == -2.78
+    assert get_mil(-10, system='metric', distance=100) == -1.2
 
 def test_get_mil_high():
-    assert get_mil(5, system='imperial') == 1.39
-    assert get_mil(5, system='metric') == 0.6
+    assert get_mil(5, system='imperial', distance=100) == 1.39
+    assert get_mil(5, system='metric', distance=100) == 0.6
 
 def test_get_mil_alt_distance():
     assert get_mil(5, system='imperial', distance=150) == 2.08
     assert get_mil(5, system='metric', distance=150) == 0.9
+
+def test_get_adjustments_high():
+    assert get_adjustments(5, 10, 'MOA') == {'sight-type': 'MOA', 'x': 2.15, 'y': 4.31}
+    assert get_adjustments(5, 10, 'MRAD') == {'sight-type': 'MRAD', 'x': 0.6, 'y': 1.2}
+
+def test_get_adjustments_low():
+    assert get_adjustments(-5, -10, 'MOA') == {'sight-type': 'MOA', 'x': -2.15, 'y': -4.31}
+    assert get_adjustments(-5, -10, 'MRAD') == {'sight-type': 'MRAD', 'x': -0.6, 'y': -1.2}
