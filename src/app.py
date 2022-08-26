3f9bf01b1d24a33f2255e6e9b5a003df203e44a4
Ewald Verhoeven <ewaldverhoeven@hotmail.com>
2022-08-26 15:20:57 +0200

    alter names

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from measurement.measures import Distance
 
-async def get_mil(delta: float, system: str, distance: int) -> float:
+async def calc_mil(delta: float, system: str, distance: int) -> float:
     #BASE_RULE: 1 mil == 3.6 inch (on 100 yards)
     BASE_RULE_DIST = 100
     BASE_RULE_DEVI = 3.6
@@ -15,7 +15,7 @@ async def get_mil(delta: float, system: str, distance: int) -> float:
         diff_factor = distance.yd / BASE_RULE_DIST
         return round(delta.inch / BASE_RULE_DEVI * diff_factor, 2)
 
-async def get_moa(delta: float, system: str, distance: int) -> float:
+async def calc_moa(delta: float, system: str, distance: int) -> float:
     # BASE_RULE: 1 moa == 1 inch (on 100 yards)
     BASE_RULE_DIST = 100
     BASE_RULE_DEVI = 1
@@ -34,11 +34,11 @@ async def get_adjustments(x_deviation: float, y_deviation: float, output: str, d
         raise ValueError("system has to be either 'metric' or 'imperial'")
 
     if output == 'MOA':
-        x = await get_moa(x_deviation, system, distance)
-        y = await get_moa(y_deviation, system, distance)
+        x = await calc_moa(x_deviation, system, distance)
+        y = await calc_moa(y_deviation, system, distance)
     elif output == 'MRAD':
-        x = await get_mil(x_deviation, system, distance)
-        y = await get_mil(y_deviation, system, distance)
+        x = await calc_mil(x_deviation, system, distance)
+        y = await calc_mil(y_deviation, system, distance)
     else:
         raise ValueError("Output must be either 'MOA' or 'MRAD'")
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,4 @@
-from src.app import get_moa, get_mil, get_adjustments
+from src.app import calc_moa, calc_mil, get_adjustments
 from hypothesis import given, strategies as st
 import pytest
 # pytest_plugins = ('pytest_asyncio',)
@@ -7,41 +7,41 @@ import pytest
 #     delta=st.floats(min_value=-25, max_value=25),
 #     system=st.sampled_from(options.SYSTEM)
 #     )
-# def test_get_moa(delta, system):
+# def test_calc_moa(delta, system):
 #     print(f'delta={delta}, system={system}')
-#     result = get_moa(delta, system)
+#     result = calc_moa(delta, system)
 #     print('test1')
 #     assert type(result) == float
 
 @pytest.mark.asyncio
-async def test_get_moa_low():
-    assert await get_moa(-10, system='imperial', distance=100) == -10
-    assert await get_moa(-10, system='metric', distance=100) == -4.31
+async def test_calc_moa_low():
+    assert await calc_moa(-10, system='imperial', distance=100) == -10
+    assert await calc_moa(-10, system='metric', distance=100) == -4.31
 
 @pytest.mark.asyncio
-async def test_get_moa_high():
-    assert await get_moa(5, system='imperial', distance=100) == 5
-    assert await get_moa(5, system='metric', distance=100) == 2.15
+async def test_calc_moa_high():
+    assert await calc_moa(5, system='imperial', distance=100) == 5
+    assert await calc_moa(5, system='metric', distance=100) == 2.15
 
 @pytest.mark.asyncio
-async def test_get_moa_alt_distance():
-    assert await get_moa(5, system='imperial', distance=150) == 7.5
-    assert await get_moa(5, system='metric', distance=150) == 3.23
+async def test_calc_moa_alt_distance():
+    assert await calc_moa(5, system='imperial', distance=150) == 7.5
+    assert await calc_moa(5, system='metric', distance=150) == 3.23
 
 @pytest.mark.asyncio
-async def test_get_mil_low():
-    assert await get_mil(-10, system='imperial', distance=100) == -2.78
-    assert await get_mil(-10, system='metric', distance=100) == -1.2
+async def test_calc_mil_low():
+    assert await calc_mil(-10, system='imperial', distance=100) == -2.78
+    assert await calc_mil(-10, system='metric', distance=100) == -1.2
 
 @pytest.mark.asyncio
-async def test_get_mil_high():
-    assert await get_mil(5, system='imperial', distance=100) == 1.39
-    assert await get_mil(5, system='metric', distance=100) == 0.6
+async def test_calc_mil_high():
+    assert await calc_mil(5, system='imperial', distance=100) == 1.39
+    assert await calc_mil(5, system='metric', distance=100) == 0.6
 
 @pytest.mark.asyncio
-async def test_get_mil_alt_distance():
-    assert await get_mil(5, system='imperial', distance=150) == 2.08
-    assert await get_mil(5, system='metric', distance=150) == 0.9
+async def test_calc_mil_alt_distance():
+    assert await calc_mil(5, system='imperial', distance=150) == 2.08
+    assert await calc_mil(5, system='metric', distance=150) == 0.9
 
 @pytest.mark.asyncio
 async def test_get_adjustments_high():
