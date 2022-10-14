6f54faf9c3ef63c37f0eef1b20d2ef309fbb2313
Ewald Verhoeven <ewald@testcoders.nl>
2022-10-14 12:05:51 +0200

    add setuptools

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_namespace_packages, setup
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='moa-mil-calculator',
+    version='0.0.1',
+    description='zero your scope quickly and correct!',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    # py_modules=[],
+    # package_dir={'': 'app'},
+    packages=find_namespace_packages(
+        include=['app', 'app.*'],
+        exclude=['__pychache__/', '.pytest_cache/']
+    ),
+    install_requires=[
+        'measurement==3.2.0',
+        'matplotlib==3.6.0'
+    ],
+    extras_require={
+        'dev': [
+            'pytest==7.1.3',
+            'pytest-reporter==0.5.2',
+            'pytest-reporter-html1==0.8.2',
+            'pytest-asyncio==0.19.0',
+            'flake8==5.0.4'
+            ]
+    }
+)
